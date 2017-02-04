f348037b880008c2f445284a1ab1a2c6f16392a0
Greg Kitson <greg.kitson@gmail.com>
2017-02-03 16:46:20 -0800

    include debian release name as tag prefix

--- release.sh
+++ release.sh
@@ -15,13 +15,16 @@ echo "$SOURCE" > "$ANNOTATION_FILE"
 echo >> "$ANNOTATION_FILE"
 curl -sI "$SOURCE" | grep '^Last-Modified\|^ETag\|^Content' >> "$ANNOTATION_FILE"
 LAST_MODIFIED_TIMESTAMP="$(grep ^Last-Modified: "$ANNOTATION_FILE" | cut -f2 -d:)"
-TAG_NAME="$($DATE -d "$LAST_MODIFIED_TIMESTAMP" '+%Y.%m.%d')"
+REF=$(git symbolic-ref -q HEAD)
+REF=${REF##refs/heads/}
+REF=${REF:-$(git rev-parse --short HEAD)}
+TAG_NAME="$REF.$($DATE -d "$LAST_MODIFIED_TIMESTAMP" '+%Y.%m.%d')"
 if git tag | grep -q "$TAG_NAME" ; then
   echo "aborting: tag $TAG_NAME already exists"
   exit 1
 else
   set -vx
-  git tag --file="$ANNOTATION_FILE" "$TAG_NAME" master
+  git tag --file="$ANNOTATION_FILE" "$TAG_NAME" HEAD
   git show "$TAG_NAME"
 fi
 
