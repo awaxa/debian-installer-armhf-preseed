8527fc2f49656c02955a40e2fbf5f0a9bd76628f
Greg Kitson <greg.kitson@gmail.com>
2017-02-03 22:55:49 -0800

    change github release presentation

--- release.sh
+++ release.sh
@@ -9,21 +9,26 @@ case $(uname -s) in
     ;;
 esac
 
-SOURCE="$(grep ^SOURCE= seed.sh | cut -f2 -d\')"
-ANNOTATION_FILE="$(mktemp)"
-echo "$SOURCE" > "$ANNOTATION_FILE"
-echo >> "$ANNOTATION_FILE"
-curl -sI "$SOURCE" | grep '^Last-Modified\|^ETag\|^Content' >> "$ANNOTATION_FILE"
-LAST_MODIFIED_TIMESTAMP="$(grep ^Last-Modified: "$ANNOTATION_FILE" | cut -f2 -d:)"
 REF=$(git symbolic-ref -q HEAD)
 REF=${REF##refs/heads/}
-REF=${REF:-$(git rev-parse --short HEAD)}
-TAG_NAME="$REF.$($DATE -d "$LAST_MODIFIED_TIMESTAMP" '+%Y.%m.%d')"
+SHORTREV="$(git rev-parse --short HEAD)"
+REF=${REF:-$SHORTREV}
+SOURCE="$(grep ^SOURCE= seed.sh | cut -f2 -d\')"
+HEADRESPONSE="$(curl -sI "$SOURCE" | grep '^Last-Modified\|^ETag\|^Content')"
+LAST_MODIFIED_TIMESTAMP="$(echo "$HEADRESPONSE" | grep ^Last-Modified: | cut -f2- -d\ )"
+ANNOTATION_FILE="$(mktemp)"
+{
+  echo 'preseed added to partition.img.gz'
+  echo
+  echo "$SOURCE"
+  echo
+  echo "$HEADRESPONSE"
+} >> "$ANNOTATION_FILE"
+TAG_NAME="$REF/$($DATE -d "$LAST_MODIFIED_TIMESTAMP" +%Y.%m.%d)"
 if git tag | grep -q "$TAG_NAME" ; then
   echo "aborting: tag $TAG_NAME already exists"
   exit 1
 else
-  set -vx
   git tag --file="$ANNOTATION_FILE" "$TAG_NAME" HEAD
   git show "$TAG_NAME"
 fi
